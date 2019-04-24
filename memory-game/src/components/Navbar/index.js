8063a48b44a27f94b9a4ee7c0d795629d808c18d
Namita Shenai <namita.shenai@gmail.com>
2019-04-23 21:35:39 -0700

    Added components NAvbar

---
+++ memory-game/src/components/Navbar/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from '../../logo.json';
+import './style.css';
+
+function Navbar(props) {
+    return (
+       <div>
+          <div className="container-fluid">
+             <div className="row">
+                <div className="col-3">
+                   <div>Memory Test Game</div>
+                </div>
+                <div className="col-6" id="game-message">
+                   <div>{props.message}</div>
+                </div>
+                <div className="col-3" id="game-score">
+                   <div>Score: {props.score} | Top Score: {props.topScore}</div>
+                </div>
+             </div>
+          </div>
+       </div>
+    );
+ }
+ 
+ export default Navbar;
