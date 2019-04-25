64846ce22a77a54d9986254f10008463e557bd9e
Namita Shenai <namita.shenai@gmail.com>
2019-04-24 18:30:43 -0700

    adding effect to the images

--- memory-game/src/components/Cards/index.js
+++ memory-game/src/components/Cards/index.js
@@ -3,8 +3,11 @@ import './style.css';
 
 function Card(props) {
     return (
-        <img className="rounded-circle" type="button" 
+    
+        <img className="rounded-circle m-3 hover11 column" type="button" 
         alt={props.name} src={props.image} onClick={() => props.id} />
+    
+        
     );
 }
 
