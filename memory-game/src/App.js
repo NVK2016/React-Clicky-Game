adc6410a52a6306f6312719659deb51ea59bee8e
Namita Shenai <namita.shenai@gmail.com>
2019-04-24 17:03:50 -0700

    Updateed  the NavBar style & codee

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import NavBar from "./components/Navbar";
+// import MemoryCard from "./components/Cards";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <NavBar />
+    );
+  }
 }
 
 export default App;
