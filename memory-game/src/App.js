215b94c15d6cd698c4e61d31b4b832e792ae6be6
Namita Shenai <namita.shenai@gmail.com>
2019-04-24 17:58:20 -0700

    Added Image cards & static avengers list

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 import NavBar from "./components/Navbar";
-// import MemoryCard from "./components/Cards";
+import MemoryCard from "./components/Cards";
+import avengerCharacters from "./avengers.json";
 
 class App extends Component {
+
+  state = {
+    // Setting this.state.avengersList to the avengers json array
+    avengersList,
+    score : 0,
+    topScore: 0
+  }
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <NavBar />
+      <div>
+        <NavBar />
+        <div className="container-fluid">
+          <MemoryCard
+            // key={avengerCharacters.id}
+          />
+        </div>
+      </div>
     );
   }
 }
