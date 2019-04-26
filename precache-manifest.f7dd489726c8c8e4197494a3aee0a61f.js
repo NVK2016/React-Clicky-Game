f3697889d772abc57c1e1f1ac86f22ead0955cf1
Namita Shenai <namita.shenai@gmail.com>
2019-04-26 10:26:10 -0700

    Updates

---
+++ precache-manifest.f7dd489726c8c8e4197494a3aee0a61f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d388646cf271932c1b723be0aa64c52",
+    "url": "/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "e09096cfcfd9dfb3d20c",
+    "url": "/React-Clicky-Game/static/css/main.90a768b2.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/React-Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "e09096cfcfd9dfb3d20c",
+    "url": "/React-Clicky-Game/static/js/main.94243dc4.chunk.js"
+  },
+  {
+    "revision": "339ef2b4a9d9b3bbcf8d",
+    "url": "/React-Clicky-Game/static/js/runtime~main.25e3a6ff.js"
+  }
+]);
